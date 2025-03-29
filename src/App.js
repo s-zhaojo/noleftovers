a593b60759eedc48975049b037cc69f3eded811a
vibhu <vibhu.rampally@gmail.com>
2025-03-29 16:50:31 -0700

    fix closing tags

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
             className="title-image" 
           />
           <h1 className="title">No Leftovers</h1>
-          <link rel="stylesheet" href="https://supportrenewalshelp.oracle.com/app/answers/detail/a_id/1246/~/pending-page?">
+          <link rel="stylesheet" href="https://supportrenewalshelp.oracle.com/app/answers/detail/a_id/1246/~/pending-page?" />
           <div class="card">
-          <img src="https://t3.ftcdn.net/jpg/00/77/71/12/360_F_77711294_BA5QTjtgGPmLKCXGdtbAgZciL4kEwCnx.jpg" alt="John" style="width:100%">
+          <img src="https://t3.ftcdn.net/jpg/00/77/71/12/360_F_77711294_BA5QTjtgGPmLKCXGdtbAgZciL4kEwCnx.jpg" alt="John" style="width:100%"/>
           <h1>John Doe</h1>
           <p class="title">CEO & Founder, Example</p>
           <p>Harvard University</p>
