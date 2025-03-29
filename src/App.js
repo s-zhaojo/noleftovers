d53bb46b753a3b6a277bcb4bad28032c116e1843
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-03-29 16:49:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,18 @@ function App() {
             className="title-image" 
           />
           <h1 className="title">No Leftovers</h1>
+          <link rel="stylesheet" href="https://supportrenewalshelp.oracle.com/app/answers/detail/a_id/1246/~/pending-page?">
+          <div class="card">
+          <img src="https://t3.ftcdn.net/jpg/00/77/71/12/360_F_77711294_BA5QTjtgGPmLKCXGdtbAgZciL4kEwCnx.jpg" alt="John" style="width:100%">
+          <h1>John Doe</h1>
+          <p class="title">CEO & Founder, Example</p>
+          <p>Harvard University</p>
+          <a href="#"><i class="fa fa-dribbble"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <p><button>Contact</button></p>
+</div>
         </div>
         <Routes>
           <Route path="/login" element={<Login />} />
