d8e5748e822ea290701e3f53ded0aacc5f3a8878
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-03-30 09:51:27 -0700

    Update Redeem.js

--- src/components/Redeem.js
+++ src/components/Redeem.js
@@ -11,6 +11,16 @@ const Redeem = () => {
       <button onClick={() => navigate('/dashboard')} className="back-button">
         Back to Dashboard
       </button>
+      <img 
+            src="https://m.media-amazon.com/images/I/71zK01gJfML.jpg" 
+            alt="No Leftovers Logo" 
+            className="title-image" 
+          />
+      <img 
+            src="https://live.staticflickr.com/65535/52765596162_1bee372f32_h.jpg" 
+            alt="No Leftovers Logo" 
+            className="title-image" 
+          />
     </div>
   );
 };
